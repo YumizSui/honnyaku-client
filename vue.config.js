a9bbcc7b4a2fef4eef512611b065e6c217a271cd
YumizSui <buggymercury@gmail.com>
2020-09-17 15:57:17 +0900

    aa

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,13 @@ module.exports = {
   devServer: {
     proxy: {
       "^/api": {
-        target: "http://honnyaku-server.yumizsui.trap.show",
-        pathRewrite: {
-          "^/api/": "/"
-        }
+        target: "http://honnyaku-server.yumizsui.trap.show"
       }
     },
     overlay: {
       warnings: true,
       errors: true
     }
-  }
+  },
+  publicPath: '/'
 };
